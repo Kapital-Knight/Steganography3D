dc52916f1e9f5ea82cc0abd007d9994721270f19
Kapital-Knight <128549645+Kapital-Knight@users.noreply.github.com>
2025-04-01 21:40:31 -0400

    Made Steganography3DTest.java to test code without running javaFX everytime.

---
+++ src/main/java/com/example/steganography3d/Steganography3DTest.java
@@ -0,0 +1,23 @@
+package com.example.steganography3d;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Steganography3DTest {
+    public static void main(String[] args) throws Exception{
+        String folderName = "OBJFiles";
+        String fileName = "cube.obj";
+
+        File file = new File(folderName + "/" + fileName);
+        System.out.println(file.getAbsolutePath());
+        System.out.println("Can read file: " + file.canRead());
+        if (!file.canRead()) {
+            throw new FileNotFoundException("Cannot read file \"" + fileName + "\"");
+        }
+
+        OBJReader reader = new OBJReader(file);
+
+        Help.about();
+        reader.getMesh().about();
+    }
+}
