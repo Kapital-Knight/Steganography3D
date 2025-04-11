d2f724c616beac5a9d61ea1d528c924fb998cc8a
Kapital-Knight <mallgaier06@gmail.com>
2025-04-11 09:45:21 -0400

    Implemented Steganographer.stringToDecimal, but doesn't handle \n or \t.

--- src/main/java/com/example/steganography3d/Steganographer.java
+++ src/main/java/com/example/steganography3d/Steganographer.java
@@ -1,17 +1,19 @@
 package com.example.steganography3d;
 
+import java.math.BigDecimal;
+
 public class Steganographer {
-    public Object3D hideMessageInObject (String message, Object3D coverObject) {
+    public static Object3D hideMessageInObject (String message, Object3D coverObject) {
         Object3D stegoObject = new Object3D();
         return stegoObject;
     }
 
-    public String readMessageFromMesh (PolygonalMesh stegoMesh) {
+    public static String readMessageFromMesh (PolygonalMesh stegoMesh) {
         String message = "";
         return message;
     }
 
-    protected String stringToDecimal (String original) throws Exception {
+    public static String stringToDecimal (String original) throws Exception {
         // All latin characters fall between these two in Unicode (except tab and newline)
         char firstAllowedCharacter = ' ';
         char lastAllowedCharacter = '~';
@@ -20,7 +22,19 @@ public class Steganographer {
             throw new Exception("Argument contains character outside of accepted range (" + firstAllowedCharacter + "," + lastAllowedCharacter + ")");
         }
 
-        return "Not implemented";
+        String decimal = "";
+
+        for (int i = 0; i < original.length(); i++) {
+            int characterCode = original.charAt(i) - firstAllowedCharacter;
+            if (characterCode <= 9) {
+                decimal += "0" + characterCode;
+            }
+            else {
+                decimal += characterCode;
+            }
+        }
+
+        return decimal;
     }
 
 
--- src/main/java/com/example/steganography3d/Steganography3DTest.java
+++ src/main/java/com/example/steganography3d/Steganography3DTest.java
@@ -5,19 +5,7 @@ import java.io.FileNotFoundException;
 
 public class Steganography3DTest {
     public static void main(String[] args) throws Exception {
-
-        String fileName = "cube.obj";
-
-        File file = new File(fileName);
-        System.out.println(file.getAbsolutePath());
-        System.out.println("Can read file: " + file.canRead());
-        if (!file.canRead()) {
-            throw new FileNotFoundException("Cannot read file \"" + fileName + "\"");
-        }
-
-        OBJReader reader = new OBJReader(file);
-
-        Help.about();
-        reader.getObject3D().getMesh().about();
+        System.out.println("Max value: " + ('~' - ' '));
+        System.out.println(Steganographer.stringToDecimal("Hello there!"));
     }
 }
