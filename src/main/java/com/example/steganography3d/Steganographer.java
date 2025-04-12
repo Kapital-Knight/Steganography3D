6f51dc6c0b4d6d7597475a8c5e1569ab5b786651
Kapital-Knight <128549645+Kapital-Knight@users.noreply.github.com>
2025-04-12 13:11:01 -0400

    Deleted commented character array.

--- src/main/java/com/example/steganography3d/Steganographer.java
+++ src/main/java/com/example/steganography3d/Steganographer.java
@@ -4,14 +4,6 @@ import java.util.Arrays;
 
 public class Steganographer {
 
-//    private static final char[] LEGAL_CHARACTERS =
-//            { '\t', '\r', '\n', ' ', '!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
-//            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-//            ':', ';', '<', '=', '>', '?', '@',
-//            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-//            '[', '\\', ']', '^', '_', '`',
-//            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-//            '{', '|', '}', '~' };
     private static final String LEGAL_CHARACTERS = "\t\r\n !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 
     public static Object3D hideMessageInObject (String message, Object3D coverObject) {
