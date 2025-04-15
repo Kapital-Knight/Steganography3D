dba4fd80acda394d41a53574f9826239035df685
Kapital-Knight <128549645+Kapital-Knight@users.noreply.github.com>
2025-04-15 00:08:13 -0400

    Changed hierarchy. No more PolygonalMesh.java or SmoothMesh.java, Instead Object3D.java uses String arrays to store all info.

--- src/main/java/com/example/steganography3d/Object3D.java
+++ src/main/java/com/example/steganography3d/Object3D.java
@@ -1,32 +1,75 @@
 package com.example.steganography3d;
 
-public class Object3D {
-    private String materialLibrary;
-    private String materialName;
-    private PolygonalMesh mesh;
+import java.util.Arrays;
 
-    public Object3D () {
-        this.mesh = new PolygonalMesh();
-    }
+public class Object3D implements Cloneable {
+    private String[] lines;
+    // regex for any 3-dimensional geometric vertex
+    private final String vertex3DRegex = "v(\\s+-?[0-9]+\\.?[0-9]*){3}";
 
-    public String getMaterialLibrary() {
-        return materialLibrary;
-    }
-    public void setMaterialLibrary(String materialLibrary) {
-        this.materialLibrary = materialLibrary;
+    public Object3D (String[] lines) {
+        this.lines = lines;
     }
 
-    public String getMaterialName() {
-        return materialName;
-    }
-    public void setMaterialName(String materialName) {
-        this.materialName = materialName;
+    public int numLines() {
+        return lines.length;
     }
 
-    public PolygonalMesh getMesh() {
-        return mesh;
+    public String[] getCoordinates(int lineIndex) {
+        String line = lines[lineIndex];
+        if (!line.matches(vertex3DRegex)) {
+            return new String[0];
+        }
+        String[] tokens = line.split("\\s+");
+        String[] coordinates = new String[tokens.length - 1];
+        System.arraycopy(tokens, 1, coordinates, 0, coordinates.length);
+        return coordinates;
     }
-    public void setMesh(PolygonalMesh mesh) {
-        this.mesh = mesh;
+
+    public boolean setCoordinates(int lineIndex, String[] newCoordinates) {
+        if (newCoordinates == null) {
+            return false;
+        }
+
+        String[] coordinates = getCoordinates(lineIndex);
+
+        if (coordinates.length != newCoordinates.length) {
+            return false;
+        }
+
+        String line = lines[lineIndex].toLowerCase();
+        String newLine = "v";
+        for (int i = 0; i < coordinates.length; i++) {
+            newLine += " " + newCoordinates[i];
+        }
+
+        lines[lineIndex] = newLine;
+        return true;
+    }
+
+    public String verticesToString() {
+        String s = "{";
+        for (int i = 0; i < lines.length; i++) {
+            String line = lines[i];
+            if (line.matches(vertex3DRegex)) {
+                s += "\n" + Arrays.toString(getCoordinates(i));
+            }
+        }
+        return s + "\n}";
+    }
+
+    @Override
+    public Object clone() {
+        try {
+            return super.clone();
+        }
+        catch (CloneNotSupportedException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + verticesToString();
     }
 }
