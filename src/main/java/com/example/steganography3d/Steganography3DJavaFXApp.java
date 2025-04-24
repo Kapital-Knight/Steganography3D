a31b1f63bc70b8fa89c010a7b0039c5630f3a400
Kapital-Knight <mallgaier06@gmail.com>
2025-04-24 19:05:51 -0400

    Reading a message is now functional in javaFX

--- src/main/java/com/example/steganography3d/Steganography3DJavaFXApp.java
+++ src/main/java/com/example/steganography3d/Steganography3DJavaFXApp.java
@@ -18,6 +18,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -27,17 +28,16 @@ import java.io.IOException;
 
 public class Steganography3DJavaFXApp extends Application {
 
-    static private StringProperty s_coverFilePath = new SimpleStringProperty();
-    static private StringProperty s_stegoFilePath = new SimpleStringProperty();
-    static private StringProperty s_textFilePath = new SimpleStringProperty();
-    static private StringProperty s_message = new SimpleStringProperty();
+    private static final StringProperty s_coverFilePath = new SimpleStringProperty();
+    private static final StringProperty s_stegoFilePath = new SimpleStringProperty();
+    private static final StringProperty s_message = new SimpleStringProperty();
 
 
     @Override
     public void start(Stage stage) throws IOException {
 
         stage.setTitle("Steganography 3D");
-        stage.setScene(hideMessageScene(stage));
+        stage.setScene(readMessageScene(stage));
 
         stage.show();
     }
@@ -52,28 +52,74 @@ public class Steganography3DJavaFXApp extends Application {
         return scene;
     }
 
-    private static Scene hideMessageScene(Stage stage) {
+    private static Scene readMessageScene(Stage stage) {
+        // Make scene half of screen
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         Pane mainPane = new StackPane();
         Scene scene = new Scene(mainPane, screenSize.width*0.5, screenSize.height*0.5);
 
-
+        // Pane for all input fields
         Pane inputPane = new VBox(10);
         inputPane.backgroundProperty().setValue(new Background(new BackgroundFill(Color.BISQUE, CornerRadii.EMPTY, Insets.EMPTY)));
         mainPane.getChildren().add(inputPane);
 
+        // Stego object field
+        FileSelectionField stegoSelectionField = new FileSelectionField("Stego Object", stage, inputPane, FileSelectionField.FileType.OBJ, false);
+        s_stegoFilePath.bind(stegoSelectionField.pathProperty());
+
+        // Confirm button to read message
+        Button confirmButton = new Button("Read message");
+        confirmButton.setOnAction(actionEvent -> {
+            try {
+                Object3D stegoObject = new OBJReader(s_stegoFilePath.getValue()).getObject3D();
+                s_message.set(Steganographer.readMessageInObject(stegoObject));
+            }
+            catch (FileNotFoundException exception) {
+                showNotification(exception.getMessage(), "File not found");
+            }
+            catch (IllegalArgumentException exception) {
+                showNotification(exception.getMessage(), "Illegal argument");
+            }
+        });
+        inputPane.getChildren().add(confirmButton);
+
+        // Message output
+        Text messageText = new Text();
+        messageText.textProperty().bind(s_message);
+        Label messageLabel = new Label("Message", messageText);
+        messageLabel.setContentDisplay(ContentDisplay.BOTTOM);
+        mainPane.getChildren().add(messageLabel);
+
+        return scene;
+    }
+
+    private static Scene hideMessageScene(Stage stage) {
+        // Make scene half of screen
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Pane mainPane = new StackPane();
+        Scene scene = new Scene(mainPane, screenSize.width*0.5, screenSize.height*0.5);
+
+        // Pane for all input fields
+        Pane inputPane = new VBox(10);
+        inputPane.backgroundProperty().setValue(new Background(new BackgroundFill(Color.BISQUE, CornerRadii.EMPTY, Insets.EMPTY)));
+        mainPane.getChildren().add(inputPane);
+
+        // Cover object field
         FileSelectionField coverSelectionField = new FileSelectionField("Cover Object", stage, inputPane, FileSelectionField.FileType.OBJ, false);
         s_coverFilePath.bind(coverSelectionField.pathProperty());
 
+        // Message field
         TextField messageField = new TextField();
         Label messageLabel = new Label("Message: ", messageField);
         messageLabel.setContentDisplay(ContentDisplay.RIGHT);
         inputPane.getChildren().add(messageLabel);
         s_message.bind(messageField.textProperty());
 
+        // Stego object field
         FileSelectionField stegoSelectionField = new FileSelectionField("Stego Object", stage, inputPane, FileSelectionField.FileType.OBJ, true);
         s_stegoFilePath.bind(stegoSelectionField.pathProperty());
 
+        // Confirm button to hide message
         Button confirmButton = new Button("Hide message");
         confirmButton.setOnAction(actionEvent -> {
             try {
