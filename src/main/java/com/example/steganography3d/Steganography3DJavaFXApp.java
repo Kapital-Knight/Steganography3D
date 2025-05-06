20682b43a26b67f95f6e50adaa8cf2dd0c126865
Kapital-Knight <mallgaier06@gmail.com>
2025-05-06 09:11:58 -0400

    Help button attempt 2

--- src/main/java/com/example/steganography3d/Steganography3DJavaFXApp.java
+++ src/main/java/com/example/steganography3d/Steganography3DJavaFXApp.java
@@ -58,7 +58,11 @@ public class Steganography3DJavaFXApp extends Application {
         s_menuScene = new Scene(mainPane, screenSize.width*0.3, screenSize.height*0.3);
         mainPane.setBackground(s_defaultBackground);
 
-        // Navigation button to hide a message
+        Button helpButton = new Button("Help");
+        helpButton.setOnAction(actionEvent -> {
+            showNotification(Help.about(), "Help");
+        });
+
         Button hideButton = new Button("Hide message");
         hideButton.setOnAction(actionEvent -> {
             stage.setScene(s_hideMessageScene);
@@ -70,8 +74,7 @@ public class Steganography3DJavaFXApp extends Application {
             stage.setScene(s_readMessageScene);
         });
 
-        // Add all nodes to mainPane
-        mainPane.getChildren().addAll(hideButton, readButton);
+        mainPane.getChildren().addAll(helpButton, hideButton, readButton);
     }
 
     // read menu and stores it in s_readMessageScene
